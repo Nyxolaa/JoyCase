3cf6df4ec0d56e2ba36586ef8e069b77b816bcf2
Blgyhn <88090162+Nyxolaa@users.noreply.github.com>
2025-03-27 01:43:49 +0300

    serilog added

---
+++ JoyCase.Api/Log/LogService.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+
+namespace JoyCase.Api.Log
+{
+    public class LogService
+    {
+        public void LogInfo(string message)
+        {
+            Serilog.Log.Information(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            Serilog.Log.Warning(message);
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            Serilog.Log.Error(ex, message);
+        }
+    }
+}
